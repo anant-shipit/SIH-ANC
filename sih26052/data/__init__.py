f45302dbd76147e6989dc1af72efc3efdfc0525a
Anant Singh Rathore <anantsinghrathore@users.noreply.github.com>
2026-09-04 05:04:35 +0530

    build: migrate setup.py to modern pyproject.toml format

--- sih26052/data/__init__.py
+++ sih26052/data/__init__.py
@@ -1,23 +1,13 @@
-from .augment import (
-    BandpassFilter,
-    GainAugment,
-    HardClipping,
-    ReverbAugment,
-    apply_augmentation_chain,
-)
-from .manifest import ManifestEntry, read_manifest, write_manifest
+from .augment import AugmentPipeline
+from .manifest import ManifestEntry, ManifestWriter, read_manifest
 from .mixer import MixerConfig, measure_snr, mix_at_snr
 from .preprocess import discover_audio_files, load_and_resample, process_directory
 
 __all__ = [
-    "BandpassFilter",
-    "GainAugment",
-    "HardClipping",
-    "ReverbAugment",
-    "apply_augmentation_chain",
+    "AugmentPipeline",
     "ManifestEntry",
+    "ManifestWriter",
     "read_manifest",
-    "write_manifest",
     "MixerConfig",
     "measure_snr",
     "mix_at_snr",
